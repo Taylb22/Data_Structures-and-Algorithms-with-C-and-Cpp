b38c9bedc40c532b409672ed29e5bb1d410d71d0
Henrique dos Santos <hsantos124.br@gmail.com>
2026-04-27 10:57:56 -0300

    Taylor Recursion Optimization

--- Recursion/taylor.c
+++ Recursion/taylor.c
@@ -17,11 +17,25 @@ double Taylor_Series(int n, int x) {
     return (pow(x, n) / factorial(n)) + Taylor_Series(n - 1, x); 
 }
 
+double e(int n, int x) {
+    static double p = 1, f = 1;
+    if (n <= 0) {
+        return 1;
+    }
+
+    double r = e(n-1, x);
+    p = x * p;
+    f = f * n;
+    return r+p / f;
+}
+
 int main () {
     int n = 5;
     int x = 5;
 
     printf("n: %d\nx: %d\nresult -> %f\n", n ,x, Taylor_Series(n, x));
+    printf("\n");
+    printf("n: %d\nx: %d\nresult -> %f\n", n ,x, e(n, x));
 
     return 0;
 }
