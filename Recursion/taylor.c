c68894b399b11de90513f3a9f2c102985a1ac59d
Henrique dos Santos <hsantos124.br@gmail.com>
2026-04-28 09:48:10 -0300

    Taylor Series Iterative Method

--- Recursion/taylor.c
+++ Recursion/taylor.c
@@ -9,6 +9,7 @@ int factorial(int n) {
     return n * factorial(n - 1);
 }
 
+//My first attempt
 double Taylor_Series(int n, int x) {
     if (n <= 0) {
         return 1;
@@ -17,6 +18,7 @@ double Taylor_Series(int n, int x) {
     return (pow(x, n) / factorial(n)) + Taylor_Series(n - 1, x); 
 }
 
+//Second attempt
 double e(int n, int x) {
     static double p = 1, f = 1;
     if (n <= 0) {
@@ -29,13 +31,40 @@ double e(int n, int x) {
     return r+p / f;
 }
 
-int main () {
-    int n = 5;
-    int x = 5;
+//Applying Horner's method
+double e_horner(int n, int x) {
+    static double s;
+    if (n == 0) {
+        return s;
+    }
+    s = 1+x*s/n;
+    return e_horner(n-1, x);
+}
 
-    printf("n: %d\nx: %d\nresult -> %f\n", n ,x, Taylor_Series(n, x));
+//Doing the Iterative way
+double ie(int n, int x) {
+    double s = 1;
+    double num = 1;
+    double den = 1;
+    for (int i = 1; i <= n; i++) {
+        num *= x;
+        den *= i;
+        s += num/den;
+    }
+    return s;
+}
+
+int main () {
+    int n = 10;
+    int x = 2;
+
+    printf("n: %d\nx: %d\n\nFirst attempt\nresult -> %lf\n", n, x, Taylor_Series(n, x));
     printf("\n");
-    printf("n: %d\nx: %d\nresult -> %f\n", n ,x, e(n, x));
+    printf("Second attempt\nresult -> %lf\n", e(n, x));
+    printf("\n");
+    printf("Horner's method\nresult -> %lf\n", e_horner(n, x));
+    printf("\n");
+    printf("Iterative method\nresult -> %lf\n", ie(n, x));
 
     return 0;
 }
