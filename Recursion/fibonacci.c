930e2d2256a356617e5e52cd147ce2b9a8284da4
Henrique dos Santos <hsantos124.br@gmail.com>
2026-04-28 21:25:51 -0300

    Fibonacci Iterative Way

--- Recursion/fibonacci.c
+++ Recursion/fibonacci.c
@@ -15,10 +15,30 @@ int fibonacci(int n){
     return fibonacci(n - 2) + fibonacci(n - 1);
 }
 
+//Iterative way
+//time O(n)
+//space O(1)
+int ifibonacci(int n) {
+    if (n <= 0) {
+        return 0;
+    }
+
+    int prev2 = 0;
+    int prev1 = 1;
+    for (int i = 1; i < n; i++) {
+        int temp = prev1;
+        prev1 = prev1 + prev2;
+        prev2 = temp;
+    }
+
+    return prev1;
+}
+
 int main() {
     int n = 10;
 
     printf("n: %d\n", n);
     printf("\nFirst attempt\nresult-> %d\n", fibonacci(n));
+    printf("Iterative way\nresult-> %d\n", ifibonacci(n));
     return 0;
 }
