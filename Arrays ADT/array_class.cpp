ca2c996384a5dc3e31b333b9981fe3514a040392
Henrique dos Santos <hsantos124.br@gmail.com>
2026-05-07 22:28:09 -0300

    Arrays ADT: added insert and erase for the array class

--- Arrays ADT/array_class.cpp
+++ Arrays ADT/array_class.cpp
@@ -12,40 +12,77 @@ class Array {
             this->arr = new T[size];
         }
 
-        void display() {
-            if (this->length <= 0) {
-                std::cout << "[]";
-                return;
-            }
-
-            std::cout << "[";
-            for (int i = 0; i<this->length-1; i++) {
-                std::cout << this->arr[i] << ", ";
-            }
-            std::cout << this->arr[this->length-1] << "]";
-        }
-
-        void append(T val) {
-            if (this->length == this->size) {
-                std::cout << "Array is full...";
-                return;
-            }
-
-            this->arr[this->length] = val;
-            this->length++;
-        }
+        void display();
+        void append(T val);
+        void insert(int index, T val);
+        void erase(int index);
 
         ~Array() {
             delete[] this->arr;
         }
 };
 
+template <typename T>
+void Array<T>::erase(int index) {
+    if (index > this->size-1 || index < 0 || index > this->length-1) {
+        throw std::runtime_error("Index out of bounds...");
+    }
+    
+    for (int i = index; i < this->length-1; i++) {
+        this->arr[i] = this->arr[i+1];
+    }
+    this->length--;
+}
+
+template <typename T>
+void Array<T>::insert(int index, T val) {
+    if (index > this->size-1 || index < 0) {
+        throw std::runtime_error("Index out of bounds...");
+    }
+    if (this->size == this->length) {
+        throw std::runtime_error("Array is full...");
+    }
+    
+    for (int i = this->length; i > index; i--) {
+        this->arr[i] = this->arr[i-1];
+    }
+    this->arr[index] = val;
+    this->length++;
+}
+
+template <typename T>
+ void Array<T>::display() {
+    if (this->length <= 0) {
+        std::cout << "[]";
+        return;
+    }
+
+    std::cout << "[";
+    for (int i = 0; i<this->length-1; i++) {
+        std::cout << this->arr[i] << ", ";
+    }
+    std::cout << this->arr[this->length-1] << "]";
+}
+
+template <typename T>
+void Array<T>::append(T val) {
+    if (this->length == this->size) {
+        throw std::runtime_error("Array is full...");
+    }
+
+    this->arr[this->length] = val;
+    this->length++;
+}
+
+
 int main () {
-    Array<int> arr(2);
-    arr.display();
+    Array<int> arr(4);
     arr.append(10);
     arr.append(20);
     arr.append(5);
+    arr.insert(2, 3);
+    arr.display();
+    arr.erase(2);
     arr.display();
 
     return 0;
